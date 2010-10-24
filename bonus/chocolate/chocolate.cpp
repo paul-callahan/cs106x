d314dd1dbef0e8bd75f40192a5eee12160e0e12d
Paul Callahan <paul.callahan@gmail.com>
2010-10-24 16:08:39 -0700

    incremental

--- bonus/chocolate/chocolate.cpp
+++ bonus/chocolate/chocolate.cpp
@@ -19,8 +19,8 @@ enum BreakageDirection {
   Vertical,
 };
 
-class Piece {
 
+class Piece {
 
 public:
   Piece(): rows(-1), cols(-1){};
@@ -63,6 +63,14 @@ struct Break {
   int pieceCols;
 };
 
+void PrintVector(string msg, vector<int> v) {
+  cout << msg << "[";
+    for (int i = 0; i < v.size(); i++) {
+      cout << v[i] << ",";
+    }
+    cout << "]" << endl;
+}
+
 void FillVector(vector<int>& source, vector<int>& dest, int bitmask) {
   for (int i = 0; i < source.size(); i++) {
     int shifted = bitmask >> i;
@@ -83,17 +91,32 @@ bool TryCombination(vector<int> shareCombo, Piece piece) {
   }
   // break the list of shares, shareCombo, into two lists.
   //do this for every non zero permutation
-  for (int subcomboInd = 1; subcomboInd < shareCombo.size(); subcomboInd++) {
+  int maxbits = (int) pow(2.0, (int)shareCombo.size());
+  for (int subcomboInd = 1; subcomboInd < maxbits -1; subcomboInd++) {
     vector<int> subcombo1;
     vector<int> subcombo2;
     FillVector(shareCombo, subcombo1, subcomboInd);  
-    FillVector(shareCombo, subcombo2, ~subcomboInd);    
+    FillVector(shareCombo, subcombo2, ~subcomboInd);
+    cout << "max bits: " << maxbits << "bitmask: " << subcomboInd << endl;
+    cout << "piece rows: " << piece.rows << " piece cols: " << piece.cols << endl;
+    PrintVector("sub1:", subcombo1);
+    PrintVector("sub2:", subcombo2);
+    //GetLine();
     for (int breakLocVert = 1; breakLocVert < piece.rows; breakLocVert++) {
       Piece subPieces[2];
       BreakPiece(subPieces, piece, Vertical, breakLocVert);    
       if (TryCombination(subcombo1, subPieces[0]) &&
         TryCombination(subcombo2, subPieces[1])) {
-          //memoization stuff
+          //memoization stuff ?
+          return true;
+      }
+    }
+    for (int breakLocHor = 1; breakLocHor < piece.rows; breakLocHor++) {
+      Piece subPieces[2];
+      BreakPiece(subPieces, piece, Horizontal, breakLocHor);    
+      if (TryCombination(subcombo1, subPieces[0]) &&
+        TryCombination(subcombo2, subPieces[1])) {
+          //memoization stuff ?
           return true;
       }
     }
@@ -104,29 +127,45 @@ bool TryCombination(vector<int> shareCombo, Piece piece) {
 
 
 
-void PrintVector(vector<int> v) {
-  cout << "[";
-    for (int i = 0; i < v.size(); i++) {
-      cout << v[i] << ",";
-    }
-    cout << "]" << endl;
-}
+
 int main() {
+
+  Piece piece(4, 3);
 
   vector<int> v;
   v.push_back(1);
   v.push_back(2);
   v.push_back(3);
+  v.push_back(6);
+
+
+  TryCombination(v, piece);
+
 
   //111
-  int maxbits = (int) pow(2.0, (int)v.size());
+
+
+
+  return 0;
+}
+
+int mainY() {
+
+  vector<int> v;
+  v.push_back(1);
+  v.push_back(2);
+  //v.push_back(3);
+  //v.push_back(6);
+
+    int maxbits = (int) pow(2.0, (int)v.size());
   for (int i = 1; i < maxbits; i++) {
     vector<int> dest;
     FillVector(v, dest, i);
-    PrintVector(dest);
+    PrintVector("a", dest);
+    dest.clear();
+    FillVector(v, dest, ~i);
+    PrintVector("b", dest);
   }
-
-
 
   return 0;
 }
